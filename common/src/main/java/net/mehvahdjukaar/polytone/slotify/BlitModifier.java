92f2c5382ee977fa6023f3d0e77015ae7fabb51b
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-04-11 13:58:25 +0200

    more stuff to sample pack

--- common/src/main/java/net/mehvahdjukaar/polytone/slotify/BlitModifier.java
+++ common/src/main/java/net/mehvahdjukaar/polytone/slotify/BlitModifier.java
@@ -1,59 +1,67 @@
 package net.mehvahdjukaar.polytone.slotify;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.*;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.mehvahdjukaar.polytone.utils.StrOpt;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
-import org.joml.Matrix4f;
 
+import java.util.List;
 import java.util.Optional;
 
-public record BlitModifier(ResourceLocation target, int index, int x, int y, int width, int height,
-                           float u1, float v1, float u2, float v2, Optional<ResourceLocation> newTexture) {
+public record BlitModifier(ResourceLocation target, int index, int x, int y, int z, int width, int height,
+                           float u0, float v0, float u1, float v1, Optional<ResourceLocation> newTexture,
+                           List<RelativeSprite> extraSprites) {
 
     public static final Codec<BlitModifier> CODEC = RecordCodecBuilder.create(i -> i.group(
             ResourceLocation.CODEC.fieldOf("texture").forGetter(BlitModifier::target),
             StrOpt.of(Codec.INT, "index", -1).forGetter(BlitModifier::index),
             StrOpt.of(Codec.INT, "x_inc", 0).forGetter(BlitModifier::x),
             StrOpt.of(Codec.INT, "y_inc", 0).forGetter(BlitModifier::y),
+            StrOpt.of(Codec.INT, "z_inc", 0).forGetter(BlitModifier::z),
             StrOpt.of(Codec.INT, "width_inc", 0).forGetter(BlitModifier::width),
             StrOpt.of(Codec.INT, "height_inc", 0).forGetter(BlitModifier::height),
-            StrOpt.of(Codec.FLOAT, "u0", -1f).forGetter(BlitModifier::u1),
-            StrOpt.of(Codec.FLOAT, "v0", -1f).forGetter(BlitModifier::v1),
-            StrOpt.of(Codec.FLOAT, "u1", -1f).forGetter(BlitModifier::u2),
-            StrOpt.of(Codec.FLOAT, "v1", -1f).forGetter(BlitModifier::v2),
-            StrOpt.of(ResourceLocation.CODEC, "new_texture").forGetter(BlitModifier::newTexture)
+            StrOpt.of(Codec.FLOAT, "u0", -1f).forGetter(BlitModifier::u0),
+            StrOpt.of(Codec.FLOAT, "v0", -1f).forGetter(BlitModifier::v0),
+            StrOpt.of(Codec.FLOAT, "u1", -1f).forGetter(BlitModifier::u1),
+            StrOpt.of(Codec.FLOAT, "v1", -1f).forGetter(BlitModifier::v1),
+            StrOpt.of(ResourceLocation.CODEC, "new_texture").forGetter(BlitModifier::newTexture),
+            StrOpt.of(RelativeSprite.CODEC.listOf(), "overlays", List.of()).forGetter(BlitModifier::extraSprites)
     ).apply(i, BlitModifier::new));
 
 
-    public void blitModified(GuiGraphics gui, ResourceLocation texture, int x1, int x2, int y1, int y2, int blitOffset,
-                             float minU, float maxU, float minV, float maxV) {
+    public void blitModified(GuiGraphics gui, TextureAtlasSprite sprite, int x1, int x2, int y1, int y2, int blitOffset) {
 
-        if (newTexture.isPresent()) texture = newTexture.get();
-        x1 += x;
+        for (RelativeSprite s : extraSprites) {
+            s.render(gui.pose(), x1, x2, y1, y2, blitOffset);
+        }
+
+        if (newTexture.isPresent()) {
+            sprite = Minecraft.getInstance().getGuiSprites().getSprite(newTexture.get());
+        }
+        float minU = u1 == -1 ? sprite.getU0() : u1;
+        float maxU = u0 == -1 ? sprite.getU1() : u0;
+        float minV = v1 == -1 ? sprite.getV0() : v1;
+        float maxV = v0 == -1 ? sprite.getV1() : v0;
+
+        blitOffset += z;
+
         int oldw = x2 - x1;
+        x1 += x;
         oldw += width;
         x2 = x1 + oldw;
 
-        y1 += y;
         int oldh = y2 - y1;
+        y1 += y;
         oldh += height;
         y2 = y1 + oldh;
 
 
-        RenderSystem.setShaderTexture(0, texture);
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        Matrix4f matrix4f = gui.pose().last().pose();
-        BufferBuilder bufferBuilder = Tesselator.getInstance().getBuilder();
-        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-        bufferBuilder.vertex(matrix4f, (float) x1, (float) y1, (float) blitOffset).uv(minU, minV).endVertex();
-        bufferBuilder.vertex(matrix4f, (float) x1, (float) y2, (float) blitOffset).uv(minU, maxV).endVertex();
-        bufferBuilder.vertex(matrix4f, (float) x2, (float) y2, (float) blitOffset).uv(maxU, maxV).endVertex();
-        bufferBuilder.vertex(matrix4f, (float) x2, (float) y1, (float) blitOffset).uv(maxU, minV).endVertex();
-        BufferUploader.drawWithShader(bufferBuilder.end());
+        SimpleSprite.blit(gui.pose().last().pose(), sprite.atlasLocation(),
+                (float) x1, (float) x2, (float) y1, (float) y2, (float) blitOffset, minU, maxU, minV, maxV);
     }
+
+
 }
---
+++ common/src/main/java/net/mehvahdjukaar/polytone/slotify/RelativeSprite.java
@@ -0,0 +1,40 @@
+package net.mehvahdjukaar.polytone.slotify;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.mehvahdjukaar.polytone.utils.StrOpt;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.resources.ResourceLocation;
+
+public record RelativeSprite(ResourceLocation texture, int x, int y, int z, int width, int height) {
+
+
+    public static final Codec<RelativeSprite> CODEC = RecordCodecBuilder.create(i -> i.group(
+            ResourceLocation.CODEC.fieldOf("texture").forGetter(RelativeSprite::texture),
+            StrOpt.of(Codec.INT, "x_inc", 0).forGetter(RelativeSprite::x),
+            StrOpt.of(Codec.INT, "y_inc", 0).forGetter(RelativeSprite::y),
+            StrOpt.of(Codec.INT, "z_inc", 0).forGetter(RelativeSprite::z),
+            StrOpt.of(Codec.INT, "width_inc", 0).forGetter(RelativeSprite::width),
+            StrOpt.of(Codec.INT, "height_inc", 0).forGetter(RelativeSprite::height)
+    ).apply(i, RelativeSprite::new));
+
+
+    public void render(PoseStack pose, int x1, int x2, int y1, int y2, int blitOffset) {
+        blitOffset += z;
+
+        int oldw = x2 - x1;
+        x1 += x;
+        oldw += width;
+        x2 = x1 + oldw;
+
+        int oldh = y2 - y1;
+        y1 += y;
+        oldh += height;
+        y2 = y1 + oldh;
+        TextureAtlasSprite sprite = Minecraft.getInstance().getGuiSprites().getSprite(texture);
+        SimpleSprite.blit(pose.last().pose(), sprite.atlasLocation(), x1, x2, y1, y2, blitOffset,
+                sprite.getU0(), sprite.getU1(), sprite.getV0(), sprite.getV1());
+    }
+}
