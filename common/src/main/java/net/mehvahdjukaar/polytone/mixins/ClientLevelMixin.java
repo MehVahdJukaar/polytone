ab693255de8c5b887d3a6686b1674f81eec27c0f
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-05-25 22:15:43 +0200

    fixed mixin

--- common/src/main/java/net/mehvahdjukaar/polytone/mixins/ClientLevelMixin.java
+++ common/src/main/java/net/mehvahdjukaar/polytone/mixins/ClientLevelMixin.java
@@ -2,9 +2,7 @@ package net.mehvahdjukaar.polytone.mixins;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.mehvahdjukaar.polytone.Polytone;
-import net.mehvahdjukaar.polytone.particle.ParticleEmitter;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.CubicSampler;
@@ -22,7 +20,7 @@ public abstract class ClientLevelMixin {
     @WrapOperation(method = "doAnimateTick", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/world/level/block/Block;animateTick(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/util/RandomSource;)V"))
     public void polytone$extraParticles(Block instance, BlockState state, Level level, BlockPos pos, RandomSource random,
-                                        Operation<Void> original){
+                                        Operation<Void> original) {
         original.call(instance, state, level, pos, random);
 
         Polytone.BLOCK_PROPERTIES.maybeEmitParticle(instance, state, level, pos);
@@ -31,8 +29,8 @@ public abstract class ClientLevelMixin {
     @WrapOperation(method = "getSkyColor", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/util/CubicSampler;gaussianSampleVec3(Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/util/CubicSampler$Vec3Fetcher;)Lnet/minecraft/world/phys/Vec3;"))
     private Vec3 polytone$modifySkyColor(Vec3 center, CubicSampler.Vec3Fetcher fetcher,
-                                       Operation<Vec3> original, @Local ClientLevel level) {
-        Vec3 modified = Polytone.DIMENSION_EFFECTS.modifySkyColor(center, level);
+                                         Operation<Vec3> original) {
+        Vec3 modified = Polytone.DIMENSION_EFFECTS.modifySkyColor(center, (ClientLevel) (Object) this);
         if (modified != null) return modified;
         return original.call(center, fetcher);
     }
