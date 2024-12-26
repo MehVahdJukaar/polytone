5bc7b5279e9456f8097dd581dee6c930d40b0fe1
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-12-26 12:18:41 +0100

    builtin maps fix

--- common/src/main/java/net/mehvahdjukaar/polytone/colormap/ColormapsManager.java
+++ common/src/main/java/net/mehvahdjukaar/polytone/colormap/ColormapsManager.java
@@ -47,6 +47,8 @@ public class ColormapsManager extends JsonImgPartialReloader {
 
     @Override
     protected void parseWithLevel(Resources resources, RegistryOps<JsonElement> ops, RegistryAccess access) {
+        addBuiltinColormaps();
+
         var jsons = resources.jsons();
         var textures = new HashMap<>(resources.textures());
 
@@ -83,12 +85,9 @@ public class ColormapsManager extends JsonImgPartialReloader {
 
     @Override
     protected void applyWithLevel(RegistryAccess access, boolean isLogIn) {
-
     }
 
-    @Override
-    protected void resetWithLevel(boolean logOff) {
-        colormaps.clear();
+    private void addBuiltinColormaps() {
         colormaps.register(ResourceLocation.parse("grass_color"), () -> GRASS_COLOR);
         colormaps.register(ResourceLocation.parse("foliage_color"), () -> FOLIAGE_COLOR);
         colormaps.register(ResourceLocation.parse("water_color"), () -> WATER_COLOR);
@@ -98,6 +97,11 @@ public class ColormapsManager extends JsonImgPartialReloader {
         colormaps.register(ResourceLocation.parse("fixed"), Colormap::createFixed);
         colormaps.register(ResourceLocation.parse("grid"), Colormap::createBiomeId);
         colormaps.register(ResourceLocation.parse("damage"), Colormap::createDamage);
+    }
+
+    @Override
+    protected void resetWithLevel(boolean logOff) {
+        colormaps.clear();
     }
 
     public void add(ResourceLocation id, Colormap colormap) {
--- common/src/main/java/net/mehvahdjukaar/polytone/slotify/GuiModifierManager.java
+++ common/src/main/java/net/mehvahdjukaar/polytone/slotify/GuiModifierManager.java
@@ -20,6 +20,7 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.level.material.Fluids;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
