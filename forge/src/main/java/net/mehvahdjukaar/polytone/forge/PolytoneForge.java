64cc70e83179fb968d5c86cb33ba11b34f097364
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-02-26 03:42:48 +0100

    a

--- forge/src/main/java/net/mehvahdjukaar/polytone/forge/PolytoneForge.java
+++ forge/src/main/java/net/mehvahdjukaar/polytone/forge/PolytoneForge.java
@@ -24,10 +24,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.TagsUpdatedEvent;
 import net.minecraftforge.event.TickEvent;
-import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
-import net.neoforged.neoforge.client.event.RenderFrameEvent;
-import net.neoforged.neoforge.client.event.ScreenEvent;
-import net.neoforged.neoforge.client.event.ViewportEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -75,7 +71,6 @@ public class PolytoneForge {
         }
     }
 
-    @SubscribeEvent
     @SubscribeEvent(priority = EventPriority.LOW)
     public void onTooltip(ItemTooltipEvent tooltipEvent) {
         var mod = ((IPolytoneItem) tooltipEvent.getItemStack().getItem()).polytone$getModifier();
