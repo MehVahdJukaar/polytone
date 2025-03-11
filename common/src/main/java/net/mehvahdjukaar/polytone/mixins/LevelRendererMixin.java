e07af4be7d3756f4b8a7e1ba974c6842765cca0b
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-03-11 14:54:27 +0100

    fixed cloud height

---
+++ common/src/main/java/net/mehvahdjukaar/polytone/mixins/LevelRendererMixin.java
@@ -0,0 +1,28 @@
+package net.mehvahdjukaar.polytone.mixins;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.mehvahdjukaar.polytone.Polytone;
+import net.mehvahdjukaar.polytone.dimension.DimensionEffectsModifier;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.LevelRenderer;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = LevelRenderer.class, priority = 1300)
+public class LevelRendererMixin {
+
+    @Shadow
+    @Nullable
+    private ClientLevel level;
+
+    @ModifyExpressionValue(method = "renderLevel",
+            require = 0,
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/client/renderer/DimensionSpecialEffects;getCloudHeight()F"))
+    private float polytone$modifyCloudHeight(float original) {
+        Float f = Polytone.DIMENSION_MODIFIERS.modifyCloudHeight(this.level);
+        return f != null ? f : original;
+    }
+}
