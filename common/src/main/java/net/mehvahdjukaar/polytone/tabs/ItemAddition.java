488c781877292b7e99fc8a6265850fe8590f3649
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-04-23 13:25:28 +0200

    item additions support holder sets

--- common/src/main/java/net/mehvahdjukaar/polytone/tabs/ItemAddition.java
+++ common/src/main/java/net/mehvahdjukaar/polytone/tabs/ItemAddition.java
@@ -12,11 +12,12 @@ import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.function.Supplier;
 
-public record ItemAddition(List<ItemStack> items, boolean inverse, ItemPredicate predicate, boolean before) {
+public record ItemAddition(Supplier<List<ItemStack>> items, boolean inverse, ItemPredicate predicate, boolean before) {
 
     public static final Codec<ItemAddition> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            ExtraItemCodecs.ITEMSTACK_OR_ITEMSTACK_LIST.fieldOf("items").forGetter(ItemAddition::items),
+            ExtraItemCodecs.ITEMSTACK_OR_LIST_OR_HOLDER_SET.fieldOf("items").forGetter(ItemAddition::items),
             Codec.BOOL.optionalFieldOf("inverse", false).forGetter(ItemAddition::inverse),
             ItemPredicate.CODEC.optionalFieldOf("predicate",  ItemPredicate.TRUE_PRED).forGetter(ItemAddition::predicate),
             Codec.BOOL.optionalFieldOf("before", false).forGetter(ItemAddition::before)
