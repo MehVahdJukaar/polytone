e1d426e9c0478e96c9517f9f1f2ab3f9ac615142
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-06-02 10:49:24 +0200

    log 2

--- common/src/main/java/net/mehvahdjukaar/polytone/utils/BiomeKeysCache.java
+++ common/src/main/java/net/mehvahdjukaar/polytone/utils/BiomeKeysCache.java
@@ -9,25 +9,28 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
 import org.jetbrains.annotations.NotNull;
 
 public class BiomeKeysCache {
 
-    private static final ResourceKey<Biome> PLAINS = ResourceKey.create(Registries.BIOME, ResourceLocation.parse("minecraft:plains"));
-
     private static final ThreadLocal<Object2ObjectOpenHashMap<Biome, ResourceKey<Biome>>> CACHE =
             ThreadLocal.withInitial(Object2ObjectOpenHashMap::new);
+
 
     public static ResourceKey<Biome> get(@NotNull Biome biome) {
         var k = CACHE.get().get(biome);
         if (k == null) {
             Level level = Minecraft.getInstance().level;
-            if (level == null) return PLAINS;
+            if (level == null) return Biomes.PLAINS;
             return CACHE.get().computeIfAbsent(biome, b ->
             {
                 var biomeKey = level.registryAccess().registryOrThrow(Registries.BIOME).getResourceKey(biome);
                 if (biomeKey.isEmpty()) {
 
+                    //we cant even log here otherwise people will complain
+                    //if you are reading this, fix your mod.
+                    return Biomes.THE_VOID;
                     //tries with server biomes. This should never happen, server biomes should never be passed here
                    // biomeKey = PlatStuff.getServerRegistryAccess()
                    //         .registryOrThrow(Registries.BIOME).getResourceKey(biome);
@@ -37,7 +40,7 @@ public class BiomeKeysCache {
                         //Polytone.LOGGER.error("Polytone detected a Server Biome was passed to a getColor client side function! This is a bug! Must be caused by some other mod!");
                        // return biomeKey.get();
                  //   } else {
-                        throw new IllegalStateException("Failed to get biome key for biome: " + biome + " This means that biome registry returned an empty key for it. How is this possible? Was it not registered? Seriously HOW? Must be due to some mod doing unsafe stuff!! This is NOT a Polytone issue!");
+                        //throw new IllegalStateException("Failed to get biome key for biome: " + biome + " This means that biome registry returned an empty key for it. How is this possible? Was it not registered? Seriously HOW? Must be due to some mod doing unsafe stuff!! This is NOT a Polytone issue!");
                  //   }
                 }
                 return biomeKey.get();
