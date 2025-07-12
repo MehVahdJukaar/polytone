516d11594f5db9abcab3d3fcaa215bb1ed9049e2
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-07-12 22:26:28 +0200

    better model rotation

--- common/src/main/java/net/mehvahdjukaar/polytone/mixins/ModelBlockRendererMixin.java
+++ common/src/main/java/net/mehvahdjukaar/polytone/mixins/ModelBlockRendererMixin.java
@@ -37,4 +37,5 @@ public abstract class ModelBlockRendererMixin {
         }
     }
 
+
 }
--- common/src/main/java/net/mehvahdjukaar/polytone/mixins/VariantDeserializerMixin.java
+++ common/src/main/java/net/mehvahdjukaar/polytone/mixins/VariantDeserializerMixin.java
@@ -2,28 +2,46 @@ package net.mehvahdjukaar.polytone.mixins;
 
 import com.google.gson.JsonObject;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.math.Transformation;
 import net.minecraft.client.renderer.block.model.Variant;
+import net.minecraft.client.resources.model.BlockModelRotation;
 import net.minecraft.util.GsonHelper;
+import net.minecraft.util.Mth;
 import org.joml.Matrix4f;
+import org.joml.Quaternionf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Variant.Deserializer.class)
 public class VariantDeserializerMixin {
 
+    @WrapOperation(method = "deserialize(Lcom/google/gson/JsonElement;Ljava/lang/reflect/Type;Lcom/google/gson/JsonDeserializationContext;)Lnet/minecraft/client/renderer/block/model/Variant;",
+    at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/model/Variant$Deserializer;getBlockRotation(Lcom/google/gson/JsonObject;)Lnet/minecraft/client/resources/model/BlockModelRotation;"))
+    public BlockModelRotation polytone$cancelVanillaTransformation(Variant.Deserializer instance, JsonObject json, Operation<BlockModelRotation> original){
+        return BlockModelRotation.by(0,0);
+    }
+
     @ModifyExpressionValue(method = "deserialize(Lcom/google/gson/JsonElement;Ljava/lang/reflect/Type;Lcom/google/gson/JsonDeserializationContext;)Lnet/minecraft/client/renderer/block/model/Variant;",
             at = @At(value = "INVOKE",
                     target = "Lnet/minecraft/client/resources/model/BlockModelRotation;getRotation()Lcom/mojang/math/Transformation;")
     )
     public Transformation polytone$addTranslation(Transformation original, @Local JsonObject jsonObject) {
-        float x = GsonHelper.getAsFloat(jsonObject, "xoffset", 0);
-        float y = GsonHelper.getAsFloat(jsonObject, "yoffset", 0);
-        float z = GsonHelper.getAsFloat(jsonObject, "zoffset", 0);
-        if (x == 0 && y == 0 && z == 0) return original;
+        float xOffset = GsonHelper.getAsFloat(jsonObject, "xoffset", 0);
+        float yOffset = GsonHelper.getAsFloat(jsonObject, "yoffset", 0);
+        float zOffset = GsonHelper.getAsFloat(jsonObject, "zoffset", 0);
+        float xRot = GsonHelper.getAsFloat(jsonObject, "x", 0);
+        float yRot = GsonHelper.getAsFloat(jsonObject, "y", 0);
+        float zRot = GsonHelper.getAsFloat(jsonObject, "z", 0);
         Matrix4f mat = new Matrix4f();
-        mat.translate(x / 16f, y / 16f, z / 16f);
-        return new Transformation(mat).compose(original);
+        Quaternionf quaternionf = (new Quaternionf())
+                .rotateYXZ(-yRot * Mth.DEG_TO_RAD,
+                        -xRot * Mth.DEG_TO_RAD, -zRot * Mth.DEG_TO_RAD);
+        mat.translate(xOffset / 16f, yOffset / 16f, zOffset / 16f);
+        mat.rotate(quaternionf);
+        return new Transformation(mat);
     }
+
 }
