776a990ae2743822cad4a9c89082e2780adf0fc0
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-07-21 21:33:31 +0200

    iris compat

--- common/src/main/java/net/mehvahdjukaar/polytone/compat/IrisCompat.java
+++ common/src/main/java/net/mehvahdjukaar/polytone/compat/IrisCompat.java
@@ -1,11 +1,18 @@
 package net.mehvahdjukaar.polytone.compat;
 
 import net.irisshaders.iris.Iris;
+import net.irisshaders.iris.api.v0.IrisApi;
+import net.irisshaders.iris.api.v0.IrisProgram;
 import net.irisshaders.iris.pipeline.ShaderRenderingPipeline;
 import net.irisshaders.iris.pipeline.WorldRenderingPipeline;
+import net.mehvahdjukaar.polytone.PolytoneRenderTypes;
 
 public class IrisCompat {
-    public IrisCompat() {
+
+    public static void init() {
+        IrisApi.getInstance().assignPipeline(PolytoneRenderTypes.LEASH_PIPELINE, IrisProgram.TEXTURED);
+        IrisApi.getInstance().assignPipeline(PolytoneRenderTypes.ADDITIVE_TRANSLUCENT_PARTICLE_PIPELINE, IrisProgram.PARTICLES_TRANSLUCENT);
+        IrisApi.getInstance().assignPipeline(PolytoneRenderTypes.ADDITIVE_TRANSLUCENT_BLOCK_PIPELINE, IrisProgram.BLOCK_TRANSLUCENT);
     }
 
     public static boolean isIrisRenderOn() {
