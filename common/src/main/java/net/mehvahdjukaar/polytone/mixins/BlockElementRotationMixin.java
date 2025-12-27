35b692ea5269a5e13c14ba3b50294a4185c33dbf
Daniel Berlin <dberlin@dberlin.org>
2025-12-27 18:55:25 -0500

    Block element rotation work for 1.21.11

--- common/src/main/java/net/mehvahdjukaar/polytone/mixins/BlockElementRotationMixin.java
+++ common/src/main/java/net/mehvahdjukaar/polytone/mixins/BlockElementRotationMixin.java
@@ -1,27 +1,23 @@
 package net.mehvahdjukaar.polytone.mixins;
 
-import net.mehvahdjukaar.polytone.misc.IExtendedBlockElementRotation;
 import net.minecraft.client.renderer.block.model.BlockElementRotation;
-import org.jetbrains.annotations.Nullable;
-import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(BlockElementRotation.class)
-public class BlockElementRotationMixin implements IExtendedBlockElementRotation {
-
-    @Unique
-    @Nullable
-    private Vector3f polytone$rotation = null;
-
-    @Nullable
-    @Override
-    public Vector3f getRotation() {
-        return this.polytone$rotation;
-    }
-
-    @Override
-    public void setRotation(Vector3f rot) {
-        this.polytone$rotation = rot;
-    }
+public class BlockElementRotationMixin  {
+//
+//    @Unique
+//    @Nullable
+//    private Vector3f polytone$rotation = null;
+//
+//    @Nullable
+//    @Override
+//    public Vector3f getRotation() {
+//        return this.polytone$rotation;
+//    }
+//
+//    @Override
+//    public void setRotation(Vector3f rot) {
+//        this.polytone$rotation = rot;
+//    }
 }
