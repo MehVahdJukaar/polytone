ce1eaa0ff15e6355ab4ba278886d88e7c8d4de3e
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-12-30 16:56:22 +0100

    fabric fix

--- fabric/src/main/java/net/mehvahdjukaar/polytone/fabric/PolytoneFabric.java
+++ fabric/src/main/java/net/mehvahdjukaar/polytone/fabric/PolytoneFabric.java
@@ -87,8 +87,6 @@ public class PolytoneFabric implements ClientModInitializer {
         });
     }
 
-    public static MinecraftServer currentServer;
-
     public static void addRenderParticlesType() {
         List<ParticleRenderType> renderOrder = new ArrayList<>(ParticleEngineAccessor.getRENDER_ORDER());
         renderOrder.add(PolytoneRenderTypes.PARTICLE_ADDITIVE_TRANSLUCENCY_RENDER_TYPE.get());
